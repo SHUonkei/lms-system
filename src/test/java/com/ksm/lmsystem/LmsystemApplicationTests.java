087af2d5341eafb8c3bbf8c58c8119e8ea415c8d
shuonkei <koyamashu3@keio.jp>
2024-07-06 19:22:30 +0900

    spring boot 導入

---
+++ src/test/java/com/ksm/lmsystem/LmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.lmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
